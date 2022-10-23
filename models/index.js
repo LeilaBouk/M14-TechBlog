b7a6cfbcfb02fd22c7b3bd2c3f5984d914e58ab9
Leila <leila.boukella@gmail.com>
2022-10-22 18:21:12 -0700

    added models and connection

---
+++ models/index.js
@@ -0,0 +1,33 @@
+const User = require('./User');
+const Posts = require('./Posts');
+const Comments = require('./Comments');
+
+// A user can have many posts
+User.hasMany(Posts, {
+    foreignKey: 'user_id',
+    onDelete: 'CASCADE'
+});
+
+// A post belongs to a user
+Posts.belongsTo(User, {
+    foreignKey: 'user_id'
+});
+
+// A user can have many comments 
+User.hasMany(Comments, {
+    foreignKey: 'user_id',
+    onDelete: 'CASCADE'
+});
+
+// A comment belongs to a user
+Comments.belongsTo(User, {
+    foreignKey: 'user_id'
+});
+
+// A post can have many comments
+Posts.hasMany(Comments, {
+    foreignKey: 'user_id',
+    onDelete: 'CASCADE'
+});
+
+module.exports = { User, Posts, Comments };

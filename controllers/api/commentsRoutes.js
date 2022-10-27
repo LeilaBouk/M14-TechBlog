14f889bb1f0447e9cd9d2cc96c7bb3e85b52a9fe
Leila <leila.boukella@gmail.com>
2022-10-26 19:53:55 -0700

    more api routes

---
+++ controllers/api/commentsRoutes.js
@@ -0,0 +1,40 @@
+const router = require('express').Router();
+const { Comments } = require('../../models');
+const withAuth = require('../../utils/auth');
+
+// Make Comments
+router.Comments('/', withAuth, async (req, res) => {
+  try {
+    const newComments = await Comments.create({
+      ...req.body,
+      user_id: req.session.user_id,
+    });
+
+    res.status(200).json(newComments);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
+// Delete Comments
+router.delete('/:id', withAuth, async (req, res) => {
+  try {
+    const CommentsData = await Comments.destroy({
+      where: {
+        id: req.params.id,
+        user_id: req.session.user_id,
+      },
+    });
+
+    if (!CommentsData) {
+      res.status(404).json({ message: 'No Comments found with this id!' });
+      return;
+    }
+
+    res.status(200).json(CommentsData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+module.exports = router;

e21bb804c324e7185fb0a03e0175574bb94a4990
Leila <leila.boukella@gmail.com>
2022-11-01 18:25:57 -0700

    added html and css for homepage

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -70,10 +70,16 @@ router.get('/profile', withAuth, async (req, res) => {
   }
 });
 
+
+router.get('/post', (req, res) => {
+
+  res.render('post');
+});
+
 router.get('/login', (req, res) => {
   // If the user is already logged in, redirect the request to another route
   if (req.session.logged_in) {
-    res.redirect('/profile');
+    res.redirect('/post');
     return;
   }
 
